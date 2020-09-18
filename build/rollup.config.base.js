e64160c27b9da3e7af670b510d8e696c9e395f7a
xxholly32 <xxholly32@163.com>
2020-09-18 14:02:43 +0800

    feat: 🎸 add rollup-plugin-esbuild

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -1,27 +1,12 @@
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve' // 告诉 Rollup 如何查找外部模块
-import commonjs from '@rollup/plugin-commonjs' // 将CommonJS模块转换为 ES2015 供 Rollup 处理
-import vue from 'rollup-plugin-vue' // 处理vue文件
-import babel from '@rollup/plugin-babel'  // rollup 的 babel 插件，ES6转ES5
-import css from 'rollup-plugin-css-only' // 提取css，压缩能力不行
-import CleanCSS from 'clean-css' // 压缩css
-import { writeFileSync } from 'fs' // 写文件
+import esbuild from "rollup-plugin-esbuild"
 
 export default {
-  input: 'packages/index.ts',
+  input: './packages/index.ts',
   external: ['vue'], //使用外部的依赖，如果使用内部 vue 会照成多重引用
   plugins: [
-    resolve({ extensions: ['.vue'] }),
-    typescript({
-      target: 'ES6',
-      module: 'ESNext',
+    esbuild({ 
+      jsxFactory: 'jsx', 
+      jsxFragment: 'Fragment' 
     }),
-    commonjs(),
-    css({ output(style) {
-      // 压缩 css 写入 dist/vue-rollup-component-template.css
-      writeFileSync('lib/index.css', new CleanCSS().minify(style).styles)
-    } }),
-    vue({ css: false }),
-    babel()
   ]
 }
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,7 @@
 import DefaultTheme from 'vitepress/dist/client/theme-default'  // To extend default theme.
 
-import vgc from '/@vgc/'
-// import vgc from '/@lib/index.esm'
+// import vgc from '/@vgc/'
+import vgc from '/@lib/index.esm'
 // import '/@lib/index.css'
 
 export default {
