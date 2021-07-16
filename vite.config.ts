29b8f306fa7f91b69349cd29e500d256295bb1bc
xiangxiao <xiangxiao3@hikvision.com.cn>
2021-07-16 19:23:18 +0800

    feat: 🎸 del lock file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
 import Layouts from "vite-plugin-vue-layouts";
 import Markdown from "vite-plugin-md";
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,7 @@ export default defineConfig({
     Markdown(),
     Layouts(),
     Pages(),
+    WindiCSS(),
   ],
   esbuild: {
     jsxFactory: "h",
